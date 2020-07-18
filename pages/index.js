de3fc7263fca0f2ed80db5965089c2d0254aeb0e
Sarah Murphy <sarahnmurphy93@gmail.com>
2020-07-18 10:32:17 +1000

    create card component to show recipe image and title

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import Head from 'next/head';
-import styles from './index.module.css';
-
+import Card from './components/card';
+import styles from './index.module.sass';
 import { getIngredientsList } from './api/ingredients';
 
 export const getStaticProps = async () => {
@@ -8,49 +7,20 @@ export const getStaticProps = async () => {
 };
 
 const Home = ({ ingredients }) => {
+  const props = {
+    image: 'https://spoonacular.com/recipeImages/474463-312x231.jpg', //'https://spoonacular.com/recipeImages/584811-312x231.jpg',
+    title: 'Southwestern Chicken Taco Pie',
+  };
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="https://vercel.com/import/nextjs" className={styles.card}>
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+    <div className={styles.cardContainer}>
+      <Card props={props} />
+      <Card props={props} />
+      <Card props={props} />
+      <Card props={props} />
+      <Card props={props} />
+      <Card props={props} />
+      <Card props={props} />
+      <Card props={props} />
     </div>
   );
 };
